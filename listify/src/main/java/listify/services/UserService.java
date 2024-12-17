60a2770c2f4701b7d3b490b15c80b6d5bd783db1
Alessandro Isceri <a.isceri@campus.unimib.it>
2024-12-17 18:59:00 +0100

    added updateToDoList method, to handle update requests from Controller.

--- listify/src/main/java/listify/services/UserService.java
+++ listify/src/main/java/listify/services/UserService.java
@@ -2,6 +2,7 @@ package listify.services;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
@@ -17,11 +18,11 @@ public class UserService {
 		// populate the database with some data
 		User aless = new User("aless@gmail.com", "pw", "aless"); //create user to test the application without DB connection
         //create 2 to-to lists to display
-        ToDoList list1 = new ToDoList("lista_progetto");
+        ToDoList list1 = new ToDoList(1, "lista_progetto");
         list1.addItem(new Activity(1, "impostare gitlab", 10, LocalDate.of(2025, 1, 20), "In Progress"));
         list1.addItem(new Activity(2, "scrivere codice", 3, LocalDate.of(2025, 4, 12), "To Do"));
         list1.addItem(new Activity(3, "scrivere test", 5, "Completed"));
-        ToDoList list2 = new ToDoList("lista_spesa"); 
+        ToDoList list2 = new ToDoList(2, "lista_spesa"); 
         aless.addToDoList(list1);
         aless.addToDoList(list2);
         users.add(aless);
@@ -45,18 +46,34 @@ public class UserService {
 		return null;
 	}
 
-	public ToDoList getToDoList(String username, String listName) {
+	public ToDoList getToDoList(String username, int listId) {
 		for(User user : users) {
 			if(username.equals(user.getUsername())){
 				List<ToDoList> lists = user.getToDoLists();
 				for(ToDoList list : lists) {
-					if(listName.equals(list.getName())){
+					if(listId == list.getId()){
 						return list;
 					}
 				}
 			}
 		}
 		return null;
+	}
+	
+	public boolean updateToDoList(String username, int listId, Activity[] updatedToDoList) {
+		for(User user : users) {
+			if(username.equals(user.getUsername())){
+				List<ToDoList> lists = user.getToDoLists();
+				for(ToDoList list : lists) {
+					if(listId == list.getId()){
+						//update the list
+						list.setToDoList(Arrays.asList(updatedToDoList));
+						return true;
+					}
+				}
+			}
+		}
+		return false;
 	}
 	
 }
