e9907bbd89082b845a1318e240653c53fe18c9c4
Alessandro Isceri <a.isceri@campus.unimib.it>
2025-01-06 14:58:39 +0100

    Moved to Java-based configuration to enable Unit test on web layer.

---
+++ listify/src/main/java/listify/config/WebConfig.java
@@ -0,0 +1,29 @@
+package listify.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "listify")
+public class WebConfig implements WebMvcConfigurer{
+
+	@Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		//to serve static resources
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/"); 
+    }
+	
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+}
